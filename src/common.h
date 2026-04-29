f6459c7cc3942455655b6413428d32460377bddb
frankd1248-hub <frankd1248@gmail.com>
2026-04-28 20:43:02 -0400

    Rebuilds

--- src/common.h
+++ src/common.h
@@ -9,8 +9,8 @@
 // #define DEBUG_TRACE_EXECUTION
 // #define DEBUG_TRACE_STACK
 
-#define DEBUG_STRESS_GC
-#define DEBUG_LOG_GC
+// #define DEBUG_STRESS_GC
+// #define DEBUG_LOG_GC
 
 #define UINT8_COUNT (UINT8_MAX + 1)
 #define ARB_COUNT (0x4000)          // Arbitrary limit so that the VM does not collapse the universe with its memory needs
